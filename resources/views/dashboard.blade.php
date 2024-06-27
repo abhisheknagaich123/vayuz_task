072dde82b6393f7b91387d313bd55a0bef271db0
Abhishek Nagaich <abhishek.nagaich_cs21@gla.ac.in>
2024-06-27 13:35:16 +0530

    new chat UI

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,34 +1,206 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    <!-- @foreach($users as $user)
-                        <p><a href="{{ route('chat', $user->id) }}">{{ $user->name }}</a></p>
-                    @endforeach -->
-                   <ul>
-    @foreach ($users as $user)
-        <li class="flex items-center justify-between mb-2">
-            <span class="text-lg">
-                <a href="{{ route('chat', $user->id) }}">{{ $user->name }}</a>
-            </span>
-            <span class="ml-2 inline-flex items-center px-2.5 py-0.5 rounded-full text-sm font-medium 
-                {{ $user->is_online ? 'bg-green-100 text-green-800' : 'bg-red-100 text-red-800' }}">
-                {{ $user->is_online ? 'Online' : 'Offline' }}
-            </span>
-        </li>
-    @endforeach
-</ul>
-
-                    
-                </div>
-            </div>
+   <!-- component -->
+<div class="flex h-screen antialiased text-gray-800">
+    <div class="flex flex-row h-full w-full overflow-x-hidden">
+      <div class="flex flex-col py-8 pl-6 pr-2 w-64 bg-white flex-shrink-0">
+        
+       
+        <div class="flex flex-col mt-8">
+          <div class="flex flex-row items-center justify-between text-xs">
+            <span class="font-bold">Active Conversations</span>
+           
+            
+          </div>
+          <div class="flex flex-col space-y-1 mt-4 -mx-2 h-48 overflow-y-auto">
+           
+            @foreach($users as $user)
+<!--                        
+            <p><a href=""></a></p> -->
+            <a href="{{ route('chat', $user->id) }}"
+              class="flex flex-row items-center hover:bg-gray-100 rounded-xl p-2"
+            >
+              <div
+                class="flex items-center justify-center h-8 w-8 bg-indigo-200 rounded-full"
+              >
+              {{ $user->name[0]}}
+              </div>
+              <div class="ml-2 text-sm font-semibold">{{ $user->name}}</div>
+            </>
+</a>
+                    @endforeach
+          
+          </div>
+          
+        
         </div>
+      </div>
+      <div class="flex flex-col flex-auto h-full p-6">
+        <div class="flex flex-col flex-auto flex-shrink-0 rounded-2xl bg-gray-100 h-full p-4" >
+          <div class="flex flex-col h-full overflow-x-auto mb-4">
+            <div class="flex flex-col h-full">
+              <div class="grid grid-cols-12 gap-y-2">
+                <div class="col-start-1 col-end-8 p-3 rounded-lg">
+                  <div class="flex flex-row items-center">
+                    <div
+                      class="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0"
+                    >
+                      A
+                    </div>
+                    <div
+                      class="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl"
+                    >
+                      <div>Hey How are you today?</div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-start-1 col-end-8 p-3 rounded-lg">
+                  <div class="flex flex-row items-center">
+                    <div
+                      class="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0"
+                    >
+                      A
+                    </div>
+                    <div
+                      class="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl"
+                    >
+                      <div>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Vel ipsa commodi illum saepe numquam maxime
+                        asperiores voluptate sit, minima perspiciatis.
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-start-6 col-end-13 p-3 rounded-lg">
+                  <div class="flex items-center justify-start flex-row-reverse">
+                    <div
+                      class="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0"
+                    >
+                      A
+                    </div>
+                    <div
+                      class="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl"
+                    >
+                      <div>I'm ok what about you?</div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-start-6 col-end-13 p-3 rounded-lg">
+                  <div class="flex items-center justify-start flex-row-reverse">
+                    <div
+                      class="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0"
+                    >
+                      A
+                    </div>
+                    <div
+                      class="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl"
+                    >
+                      <div>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing. ?
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-start-1 col-end-8 p-3 rounded-lg">
+                  <div class="flex flex-row items-center">
+                    <div
+                      class="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0"
+                    >
+                      A
+                    </div>
+                    <div
+                      class="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl"
+                    >
+                      <div>Lorem ipsum dolor sit amet !</div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-start-6 col-end-13 p-3 rounded-lg">
+                  <div class="flex items-center justify-start flex-row-reverse">
+                   
+                   
+                  </div>
+                </div>
+                
+               
+              </div>
+            </div>
+          </div>
+          <div
+            class="flex flex-row items-center h-16 rounded-xl bg-white w-full px-4"
+          >
+            <div>
+              <button
+                class="flex items-center justify-center text-gray-400 hover:text-gray-600"
+              >
+                <svg
+                  class="w-5 h-5"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"
+                  ></path>
+                </svg>
+              </button>
+            </div>
+            <div class="flex-grow ml-4">
+              <div class="relative w-full">
+                <input
+                  type="text"
+                  class="flex w-full border rounded-xl focus:outline-none focus:border-indigo-300 pl-4 h-10"
+                />
+                <button
+                  class="absolute flex items-center justify-center h-full w-12 right-0 top-0 text-gray-400 hover:text-gray-600"
+                >
+                  <svg
+                    class="w-6 h-6"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                    ></path>
+                  </svg>
+                </button>
+              </div>
+            </div>
+            <div class="ml-4">
+              <button
+                class="flex items-center justify-center bg-indigo-500 hover:bg-indigo-600 rounded-xl text-white px-4 py-1 flex-shrink-0"
+              >
+                <span>Send</span>
+                <span class="ml-2">
+                  <svg
+                    class="w-4 h-4 transform rotate-45 -mt-px"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
+                    ></path>
+                  </svg>
+                </span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </x-app-layout>
--- resources/views/livewire/chat-component.blade.php
+++ resources/views/livewire/chat-component.blade.php
@@ -1,105 +1,116 @@
-<div>
-<div style="overscroll-behavior: none;">
-      <div
-        class="fixed w-full bg-green-400 h-16 pt-2 text-white flex justify-between shadow-md"
-        style="top:0px; overscroll-behavior: none;"
-      >
-        <!-- back button -->
-        <a href="/dashboard">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            class="w-12 h-12 my-1 text-green-100 ml-2"
-          >
-            <path
-              class="text-green-100 fill-current"
-              d="M9.41 11H17a1 1 0 0 1 0 2H9.41l2.3 2.3a1 1 0 1 1-1.42 1.4l-4-4a1 1 0 0 1 0-1.4l4-4a1 1 0 0 1 1.42 1.4L9.4 11z"
-            />
-          </svg>
-        </a>
-        <div class="my-3 text-green-100 font-bold text-lg tracking-wide">{{ $user->name }}</div>
-        <!-- 3 dots -->
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          class="icon-dots-vertical w-8 h-8 mt-2 mr-2"
-        >
-          <path
-            class="text-green-100 fill-current"
-            fill-rule="evenodd"
-            d="M12 7a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 7a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 7a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"
-          />
+<div class="flex h-screen antialiased text-gray-800">
+    
+  <a href="/dashboard">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-12 h-12 my-1 text-black ml-2 font-bold">
+      <path class="fill-current"
+        d="M9.41 11H17a1 1 0 0 1 0 2H9.41l2.3 2.3a1 1 0 1 1-1.42 1.4l-4-4a1 1 0 0 1 0-1.4l4-4a1 1 0 0 1 1.42 1.4L9.4 11z" />
+    </svg>
+  </a>
+  <h1 class="text-center font-bold text-2xl">Chat With {{ $user->name }}</h1>
+
+
+  <div class="flex flex-row h-full w-full overflow-x-hidden">
+   
+    <div class="flex flex-col flex-auto h-full p-6">
+      <div class="flex flex-col flex-auto flex-shrink-0 rounded-2xl bg-gray-100 h-full p-4">
+        <div class="flex flex-col h-full overflow-x-auto mb-4">
+          <div class="flex flex-col h-full">
+            <div class="grid grid-cols-12 gap-y-2">
+
+              @foreach($messages as $message)
+          @if($message['sender'] != auth()->user()->name)
+
+        <div class="col-start-1 col-end-8 p-3 rounded-lg">
+        <div class="flex flex-row items-center">
+          
+        <div class="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
+        {{ substr($message['sender'], 0, 1) }}
+
+        </div>
+        <button class="text-red-600 mx-2" wire:click="deleteMessage({{ $message['id'] }})">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-5 h-5">
+          <path class="fill-current"
+          d="M9 3v1H4v2h16V4h-5V3H9zm3 5c-.552 0-1 .448-1 1v7c0 .552.448 1 1 1s1-.448 1-1V9c0-.552-.448-1-1-1zm-4 0c-.552 0-1 .448-1 1v7c0 .552.448 1 1 1s1-.448 1-1V9c0-.552-.448-1-1-1zm8 0c-.552 0-1 .448-1 1v7c0 .552.448 1 1 1s1-.448 1-1V9c0-.552-.448-1-1-1z" />
         </svg>
+        </button>
+        <div class="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl">
+        <div>{{ $message['message'] }}</div>
+        </div>
+        </div>
+        </div>
+
+      @else
+
+      <div class="col-start-6 col-end-13 p-3 rounded-lg">
+      <div class="flex items-center justify-start flex-row-reverse">
+      <div class="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">
+      A
+      </div>
+      <button class="text-red-600 mx-2" wire:click="deleteMessage({{ $message['id'] }})">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-5 h-5">
+        <path class="fill-current"
+        d="M9 3v1H4v2h16V4h-5V3H9zm3 5c-.552 0-1 .448-1 1v7c0 .552.448 1 1 1s1-.448 1-1V9c0-.552-.448-1-1-1zm-4 0c-.552 0-1 .448-1 1v7c0 .552.448 1 1 1s1-.448 1-1V9c0-.552-.448-1-1-1zm8 0c-.552 0-1 .448-1 1v7c0 .552.448 1 1 1s1-.448 1-1V9c0-.552-.448-1-1-1z" />
+      </svg>
+      </button>
+      <div class="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl">
+      <div>{{ $message['message'] }}</div>
+
+      </div>
+      </div>
       </div>
 
-      <div class="mt-20 mb-16">
-        @foreach($messages as $message)
-
-        @if($message['sender'] != auth()->user()->name)
-        <div class="clearfix w-4/4">
-          <div
-            class="bg-gray-300 mx-4 my-2 p-2 rounded-lg inline-block"
-          ><b>{{ $message['sender'] }} : </b>{{ $message['message'] }}</div>
-          <button class="text-gray-600 mx-2" wire:click="editMessage({{ $message['id'] }})">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-5 h-5">
-                  <path class="fill-current" d="M15.232 5.232l3.536 3.536L7.536 20H4v-3.536L15.232 5.232zM19.768 3.768a1.414 1.414 0 0 0-2-2l-1.536 1.536 3.536 3.536 1.536-1.536a1.414 1.414 0 0 0 0-2z"/>
-                </svg>
-              </button>
-              <button class="text-red-600 mx-2" wire:click="deleteMessage({{ $message['id'] }})">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-5 h-5">
-                  <path class="fill-current" d="M9 3v1H4v2h16V4h-5V3H9zm3 5c-.552 0-1 .448-1 1v7c0 .552.448 1 1 1s1-.448 1-1V9c0-.552-.448-1-1-1zm-4 0c-.552 0-1 .448-1 1v7c0 .552.448 1 1 1s1-.448 1-1V9c0-.552-.448-1-1-1zm8 0c-.552 0-1 .448-1 1v7c0 .552.448 1 1 1s1-.448 1-1V9c0-.552-.448-1-1-1z"/>
-                </svg>
-              </button></div>
-        @else
-        <div class="clearfix w-4/4">
-            <div class="text-right">
-                <p class="bg-green-300 mx-4 my-2 p-2 rounded-lg inline-block">{{ $message['message'] }} <b>: You</b></p><button class="text-gray-600 mx-2" wire:click="editMessage({{ $message['id'] }})">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-5 h-5">
-                  <path class="fill-current" d="M15.232 5.232l3.536 3.536L7.536 20H4v-3.536L15.232 5.232zM19.768 3.768a1.414 1.414 0 0 0-2-2l-1.536 1.536 3.536 3.536 1.536-1.536a1.414 1.414 0 0 0 0-2z"/>
-                </svg>
-              </button>
-              <button class="text-red-600 mx-2" wire:click="deleteMessage({{ $message['id'] }})">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-5 h-5">
-                  <path class="fill-current" d="M9 3v1H4v2h16V4h-5V3H9zm3 5c-.552 0-1 .448-1 1v7c0 .552.448 1 1 1s1-.448 1-1V9c0-.552-.448-1-1-1zm-4 0c-.552 0-1 .448-1 1v7c0 .552.448 1 1 1s1-.448 1-1V9c0-.552-.448-1-1-1zm8 0c-.552 0-1 .448-1 1v7c0 .552.448 1 1 1s1-.448 1-1V9c0-.552-.448-1-1-1z"/>
-                </svg>
-              </button>
-            </div>
-        </div>
-        @endif
-
-
+       @endif
         @endforeach
 
 
+
+            </div>
+          </div>
+        </div>
+        <form wire:submit.prevent="sendMessage()">
+          <div class="flex flex-row items-center h-16 rounded-xl bg-white w-full px-4">
+            <div>
+              <button type="button" class="flex items-center justify-center text-gray-400 hover:text-gray-600">
+                <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13">
+                  </path>
+                </svg>
+              </button>
+            </div>
+            <div class="flex-grow ml-4">
+              <div class="relative w-full">
+                <input type="text"
+                  class="flex w-full border rounded-xl focus:outline-none focus:border-indigo-300 pl-4 h-10"
+                  wire:model="message" placeholder="Type your message here..." />
+                <button type="submit"
+                  class="absolute flex items-center justify-center h-full w-12 right-0 top-0 text-gray-400 hover:text-gray-600">
+                  <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                  </svg>
+                </button>
+              </div>
+            </div>
+            <div class="ml-4">
+              <button type="submit"
+                class="flex items-center justify-center bg-indigo-500 hover:bg-indigo-600 rounded-xl text-white px-4 py-1 flex-shrink-0">
+                <span>Send</span>
+                <span class="ml-2">
+                  <svg class="w-4 h-4 transform rotate-45 -mt-px" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"></path>
+                  </svg>
+                </span>
+              </button>
+            </div>
+          </div>
+        </form>
+
       </div>
     </div>
-
-    <form wire:submit="sendMessage()">
-        <div class="fixed w-full flex justify-between bg-green-100" style="bottom: 0px;">
-        <textarea
-            class="flex-grow m-2 py-2 px-4 mr-1 rounded-full border border-gray-300 bg-gray-200 resize-none"
-            rows="1"
-            wire:model="message"
-            placeholder="Message..."
-            style="outline: none;"
-        ></textarea>
-        <button class="m-2" type="submit" style="outline: none;">
-            <svg
-            class="svg-inline--fa text-green-400 fa-paper-plane fa-w-16 w-12 h-12 py-2 mr-2"
-            aria-hidden="true"
-            focusable="false"
-            data-prefix="fas"
-            data-icon="paper-plane"
-            role="img"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
-            >
-            <path
-                fill="currentColor"
-                d="M476 3.2L12.5 270.6c-18.1 10.4-15.8 35.6 2.2 43.2L121 358.4l287.3-253.2c5.5-4.9 13.3 2.6 8.6 8.3L176 407v80.5c0 23.6 28.5 32.9 42.5 15.8L282 426l124.6 52.2c14.2 6 30.4-2.9 33-18.2l72-432C515 7.8 493.3-6.8 476 3.2z"
-            />
-            </svg>
-        </button>
-        </div>
-    </form>
+  </div>
 </div>
